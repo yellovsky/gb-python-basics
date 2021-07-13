f1329a19c5df1e883f76e25d56bba5785b7e1136
Dmitry Elovsky <dmitry.elovsky@gmail.com>
2021-07-13 17:06:53 +0300

    Lesson 7 - Task 1
    Matrix class

---
+++ lesson-7/task-1/__main__.py
@@ -0,0 +1,33 @@
+def add_rows(row_a, row_b):
+    if (len(row_a) != len(row_b)):
+        raise TypeError('Row lens mus be equal')
+
+    return [row_a[i] + row_b[i] for i in range(len(row_a))]
+
+
+class Matrix:
+    rows = []
+
+    def __init__(self, rows):
+        row_dimensions = [len(row) for row in rows]
+
+        for index, d in enumerate(row_dimensions[1:]):
+            if (d != row_dimensions[index]):
+                raise TypeError('It\'s not a matrix')
+
+        self.rows = rows
+
+    def __add__(self, other):
+        self_dimensions = self.get_dimensions()
+        other_dimensions = other.get_dimensions()
+
+        if (self_dimensions != other_dimensions):
+            raise TypeError('Matices must have same dimensions')
+
+        rows = [add_rows(self.rows[i], other.rows[i])
+                for i in range(self.get_dimensions()[0])]
+
+        return Matrix(rows)
+
+    def get_dimensions(self):
+        return len(self.rows), len(self.rows[0])
